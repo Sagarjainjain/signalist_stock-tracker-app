66c96b28c6683cd87b75218093813fe285f1cf43
Sagarjainjain <sagarjain.jain10@gmail.com>
2026-01-17 15:51:26 +0530

    implement homepage with TradingView widget

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,60 @@
-import { Button } from "@/components/ui/button"
+import Tradingviewwidget from "@/components/tradingviewwidget";
+import { Button } from "@/components/ui/button";
+import {
+  HEATMAP_WIDGET_CONFIG,
+  MARKET_DATA_WIDGET_CONFIG,
+  MARKET_OVERVIEW_WIDGET_CONFIG,
+  TOP_STORIES_WIDGET_CONFIG,
+} from "@/lib/constants";
 
 const Home = () => {
+
+  const scriptUrl =
+    "https://s3.tradingview.com/external-embedding/embed-widget-";
   return (
     <div className="flex min-h-screen home-wrapper">
-      <h1>hello</h1>
-    </div>
-  )
-}
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <Tradingviewwidget
+            title="Market Overview"
+            scriptUrl={`${scriptUrl}market-overview.js`}
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+        <div className="md:col-span xl:col-span-2">
+          <Tradingviewwidget
+            title="Stock Heatmap"
+            scriptUrl={`${scriptUrl}stock-heatmap.js`}
+            config={HEATMAP_WIDGET_CONFIG}
 
-export default Home
+            height={600}
+          />
+        </div>
+      </section>
+      <section className="grid w-full gap-8 home-section">
+        <div className="h-full md:col-span-1 xl:col-span-1">
+          <Tradingviewwidget
+     
+            scriptUrl={`${scriptUrl}timeline.js`}
+            config={TOP_STORIES_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+        <div className="h-full md:col-span-1 xl:col-span-2">
+          <Tradingviewwidget
+       
+            scriptUrl={`${scriptUrl}market-quotes.js`}
+            config={MARKET_DATA_WIDGET_CONFIG}
+   
+            height={600}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
